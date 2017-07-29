4ee0340fdcfa55415ca74dd685f03fd51806ef16
kongchenglc@gmail.com <kongchenglc@gmail.com>
2017-07-29 23:27:32 +0800

    height_bug repair

--- js/user-center.js
+++ js/user-center.js
@@ -1,16 +1,3 @@
-window.onload = function() {
-    contentWrapperMinHeight();
-}
-
-window.onresize = function() {
-    setTimeout(contentWrapperMinHeight, 1);
-}
-
-function contentWrapperMinHeight() {
-    var contentWrapper = document.getElementsByClassName("content-wrapper")[0];
-    contentWrapper.style.minHeight = 634 + "px";
-}
-
 function submit_password() {
     var passwordWarning = document.getElementById("passwordWarning");
     var password = document.getElementById("password");
